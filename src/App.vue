8d9e640b005509fdbff73e502f152a39a30e294a
Prateek <prateek.negi@gmail.com>
2017-02-21 00:48:27 -0500

    Change / to go to Home component

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,28 @@
 </style>
 
 <template>
+  <div id="wrapper" >
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <i class="glyphicon glyphicon-time"></i>
+          Vue Time Tracker
+        </a>
+        <ul class="nav navbar-nav">
+          <li><a v-link="'/home'">Home</a></li>
+          <li><a v-link="'/time-entries'">Time Entries</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="col-sm-3">
 
-<div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-</div>
-
+      </div>
+      <div class="col-sm-9">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
